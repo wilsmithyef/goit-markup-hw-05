f19ee862b0346f25d355e0e0790b03b759616cba
wilsmithyef <wilsmithyef@gmail.com>
2023-05-30 15:59:35 -0500

    one advance hw 05

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+(()==> {
+const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    backdrop: document.querySelector("[data-backdrop]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+      refs.backdrop.classList.toggle("is-hidden");
+      refs.modal.classList.toggle("is-hidden");
+  }
+})()
